8b04ce6432767d7db432e0f9fb3be9ae099dc4ab
Ricardo Licea <ricardo.licea@endava.com>
2024-10-02 14:48:58 -0600

    Terminamos neustro proyecto de lista de tareas ane angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'listaTareasApp';
+export class AppComponent implements OnInit {
+  listaTareas:string[] = [];
+  nuevaTarea: string = '';
+
+  private _tareaService = inject(TareasService);
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareaService.getTareas();
+  }
+
+  eliminarTarea(index: number){
+    this._tareaService.eliminarTareas(index);
+    this.listaTareas = this._tareaService.getTareas();
+  }
+
+  agregarTarea(){
+    this._tareaService.agregaTareas(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareaService.getTareas();
+  }
+
+
+
 }
